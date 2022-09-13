d5d42b57aae7bcedfef0ee1e537eb2dff9026242
achraf <achraf.ezzaam@gmail.com>
2022-09-13 21:21:34 +0100

    test

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://abc61e874792243328ed48cafbbaefb5-268414917.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
